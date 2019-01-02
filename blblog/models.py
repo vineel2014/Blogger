4c0e18beff6fa74feca910018beacbbf82aa7583
vineel2019 <vineel2006@gmail.com>
2019-01-02 12:47:18 +0530

    My Django Blog

---
+++ blblog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+# Create your models here.
+
+class Post(models.Model):
+
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    created_date=models.DateTimeField(default=timezone.now)
+    Plubished_date=models.DateTimeField(blank=True, null=True)
+    Owner=models.CharField(max_length=10,default="Sairam")
+
+    def publish(self):
+       self.published_date=timezone.now()
+       self.save()
+
+    def __str__(self):    
+       return self.title 
